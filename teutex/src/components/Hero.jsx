3a57f9cd2cdbdd4c362d0d73427b1087c634bc89
pelinkovacpapi <114080264+PelinkovacPapi@users.noreply.github.com>
2023-03-02 10:22:43 +0100

    Updated Hero

--- teutex/src/components/Hero.jsx
+++ teutex/src/components/Hero.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import styles from '../style'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className={`flex md:flex-row flex-col text-white font-thin text-6xl mt-10 font-sans ${styles.paddingY}`}>
+      <div>
+        Welcome to Our{'\n'}
+        <div>
+        Company!
+        </div>
+        <div className='text-[14px] mt-5'>
+        Are you looking for reliable immigration consultants to handle your immigration case?
+        <div className='mt-1'>
+        Our certified and reliable Immigration Consultant professionals can help you!          
+        </div>
+        <div className='mt-1'>
+        We provide services in all different areas of immigration.          
+        </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
